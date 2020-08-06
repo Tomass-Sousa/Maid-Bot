80c0f5152d9078bf07247954f5e7d57ac0bb75f3
ToastAs <tomasousamix@gmail.com>
2020-08-06 11:21:12 +0200

    Maid

--- Maid.js
+++ Maid.js
@@ -301,3 +301,27 @@ client.on('message', message => {
 //login
 client.login(DISCORD_TOKEN);
 
+//Boosters
+const Boosters = new discord.MessageEmbed()
+    .setColor('#F0D2EA')
+    .setAuthor('Les boosters :', 'https://cdn.discordapp.com/attachments/648412438219325461/724619286924230666/a_762309dc83e08f460fd3c269aeaf8f3c.gif' )
+    .setDescription('__En boostant le serveur vous aurez les avantages suivants:__')
+    .addField(":cherry_blossom: 1 ⊱", "Un rôle dj.")
+    .addField(":cherry_blossom: 2 ⊱", "Un rôle personnalisé.")
+    .addField(":cherry_blossom: 3 ⊱", "Une couleur au choix.")
+    .addField(":cherry_blossom: 4 ⊱", "Une priorité en vocal.")
+    .addField(":cherry_blossom: 5 ⊱", "Ajouter des emotes supplémentaires.")
+    .addField(":cherry_blossom: 6 ⊱", "Place prioritaire pour les events et autres.")
+    .addField(":cherry_blossom: 7 ⊱", "Envoyer des liens et des images sur les channels.")
+    .addField(":cherry_blossom: 8 ⊱", "Avoir une place tout en haut de la liste des membres.")
+    .addField(":cherry_blossom: 9 ⊱", "Contacter le staff plus facilement, en mp ou a l'écrit.")
+    .setImage('https://cdn.discordapp.com/attachments/711302491941371924/740860966543163452/Anime-Girl-Cherry-Blossom-HD-Wallpaper-Background-Image-.jpg')
+    
+  client.on('message', message => {
+    if (!message.guild) return;
+
+    if (message.content === 'fziuvneivnediez') {
+       message.channel.send(Boosters);
+    }
+});    
+
