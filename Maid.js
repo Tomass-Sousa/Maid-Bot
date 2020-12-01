227e181d08b8928d39db510e6a45922c13e92a7e
ToastAs <tomasousamix@gmail.com>
2020-12-01 13:19:42 +0100

    Maid
    Discord Bot

--- Maid.js
+++ Maid.js
@@ -426,6 +426,72 @@ function play(guild, song) {
   serverQueue.textChannel.send(" `"+ song.title + "` est actuellement joué");
 }
 
+//Starboard 
+client.on('messageReactionAdd', async (reaction, user) => {
+  const handleStarboard = async () => {
+      const maidboard = client.channels.cache.find(channel => channel.name.toLowerCase() === '〢:white_flower:starboard');
+      const msgs = await  maidboard.messages.fetch({ limit: 100 });
+      const existingMsg = msgs.find(msg => 
+          msg.embeds.length === 1 ?
+          (msg.embeds[0].footer.text.startsWith(reaction.message.id) ? true : false) : false);
+      if(existingMsg) existingMsg.edit(`${reaction.count} - 🌸`);
+      else {
+          const image = reaction.message.attachments.size > 0 ? (reaction, reaction.message.attachments.array()[0].url) : '';
+          const embed = new discord.MessageEmbed()
+              .setAuthor(reaction.message.author.tag, reaction.message.author.displayAvatarURL())
+              .setColor(`#ffdfdf`)
+              .addFields({
+                  name: '~',
+                  value:
+                   `**[Cliquez ici pour voir le message](https://discordapp.com/channels/${reaction.message.guild.id}/${reaction.message.channel.id}/${reaction.message.id})**`
+              })
+              
+              .setDescription(reaction.message.content)
+              .setImage(image)
+              .setFooter(reaction.message.id)
+              .setTimestamp();
+          if(maidboard)
+              maidboard.send('1 - 🌸', embed);
+      }
+  }
+  if(reaction.emoji.name === '⭐') {
+      if(reaction.message.channel.name.toLowerCase() === '〢:white_flower:starboard') return;
+      if(reaction.message.partial) {
+          await reaction.fetch();
+          await reaction.message.fetch();
+          handleStarboard();
+      }
+      else
+          handleStarboard();
+  }
+});
+
+client.on('messageReactionRemove', async (reaction, user) => {
+  const handleStarboard = async () => {
+      const maidboard = client.channels.cache.find(channel => channel.name.toLowerCase() === '〢:white_flower:starboard');
+      const msgs = await  maidboard.messages.fetch({ limit: 100 });
+      const existingMsg = msgs.find(msg => 
+          msg.embeds.length === 1 ? 
+          (msg.embeds[0].footer.text.startsWith(reaction.message.id) ? true : false) : false);
+      if(existingMsg) {
+          if(reaction.count === 0)
+              existingMsg.delete({ timeout: 2500 });
+          else
+              existingMsg.edit(`${reaction.count} - ⭐`)
+      };
+  }
+  if(reaction.emoji.name === '⭐') {
+      if(reaction.message.channel.name.toLowerCase() === '〢:white_flower:starboard') return;
+      if(reaction.message.partial) {
+          await reaction.fetch();
+          await reaction.message.fetch();
+          handleStarboard();
+      }
+      else
+          handleStarboard();
+  }
+});
+
 //login
 client.login(DISCORD_TOKEN);  
 
